bd0705c40398055c448e2f5b13240823ebe0f44e
Scott Hawley <shawley@provision.local>
2015-09-02 22:36:53 -0500

    With UIImageViews, you are expected to provide a separate "highlighted image" if you want to do highlighting.  If this is unknown, then the way to do it is to put a semi-transparent shape (e.g. a translucent blue rectangle) overtop of the image. That is what this demo achieves.

--- HighLightingDemo/ViewController.swift
+++ HighLightingDemo/ViewController.swift
@@ -10,6 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myObjectView: UIImageView!
+    
+    
+    @IBOutlet weak var myActionButton: UIButton!
+    
+    var highlighted = false
+    var coverView : UIView!
+    
+    func getRealFrameForImage(view: UIImageView) -> CGRect {
+        let aspect : CGFloat = CGFloat(view.image!.size.width) / view.image!.size.height
+        
+        let scaledHeight = view.frame.width / aspect
+        let frame = CGRectMake(view.frame.minX, view.center.y - scaledHeight/2, view.frame.width, scaledHeight)
+        return frame
+    }
+    func toggleHighlight(view: UIView) {
+        highlighted = !highlighted
+        
+        if highlighted {  //make a new box
+           // make a copy of the original image
+            print("Making new view for hightlighting")
+            coverView = UIView(frame: getRealFrameForImage(view as! UIImageView))
+            coverView.backgroundColor = UIColor.blueColor()
+            coverView.alpha = 0.5
+            view.superview!.addSubview(coverView)
+            view.setNeedsDisplay()
+            coverView.setNeedsDisplay()
+        } else {
+            if nil != coverView {
+                //coverView.alpha = 0.0
+                coverView.removeFromSuperview()
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func myActionButtonPress(sender: AnyObject) {
+        print("You pushed the button!")
+        toggleHighlight(myObjectView)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
